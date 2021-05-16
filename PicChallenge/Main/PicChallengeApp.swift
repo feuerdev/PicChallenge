337b91e96c3ec8cdd660ee1411a4f9a7ecaa224c
Jannik Feuerhahn <jannik@feuer.dev>
2021-05-16 14:18:03 +0800

    created main package

---
+++ PicChallenge/Main/PicChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicChallengeApp.swift
+//  PicChallenge
+//
+//  Created by Jannik Feuerhahn on 03.05.21.
+//
+
+import SwiftUI
+
+@main
+struct PicChallengeApp: App {    
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
